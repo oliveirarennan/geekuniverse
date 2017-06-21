387906414fabf7df6276e4281506b189f6f74033
YagoOliveira <51115152016.1@51DE150146.senacrjedu.local>
2017-06-21 19:19:11 -0300

    Modelo  -Categoria  -Fabricante Dao  -Fabricante  -Categoria

---
+++ geekUniverse/src/modelo/Categoria.java
@@ -0,0 +1,27 @@
+package modelo;
+
+public class Categoria {
+	
+	private int id;
+	private String nome;
+	private String status;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
